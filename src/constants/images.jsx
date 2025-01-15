2c797d9aac347d8daa4787acaed676c1268d67ef
Charles Mwangi <cmkaranja97@gmail.com>
2025-01-15 10:27:36 +0300

    Correction

--- src/constants/images.jsx
+++ src/constants/images.jsx
@@ -1,6 +1,6 @@
 import about from "../assets/About.jpeg";
 import logo from "../assets/Logo.png";
-import manager from "../assets/manager.jpg";
+import manager from "../assets/Manager.jpg";
 import products from "../assets/Products.jpg";
 
 export default {
